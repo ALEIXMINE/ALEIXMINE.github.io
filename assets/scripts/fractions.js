f8b96db092c827c45d80e2b16a1cdefd3fe28dd3
ALEIXMINE <87977748+ALEIXMINE@users.noreply.github.com>
2022-05-24 21:49:17 +0200

    master

--- assets/scripts/fractions.js
+++ assets/scripts/fractions.js
@@ -35,13 +35,38 @@ const sumFrac = (a, b) => {
     return [resNumer/greatestDivisor, resDenom/greatestDivisor];
 };
 
-function get_inputs() {return document.getElementById("input1").value,document.getElementById("input2").value,document.getElementById("input3").valuedocument.getElementById("input4").value}
+const substractFrac = (a, b) => {
+    const aDenom = a[1], aNumer = a[0];
+    const bDenom = b[1], bNumer = b[0];
+    let resDenom = aDenom * bDenom;
+    let resNumer = (aDenom*bNumer) - (bDenom*aNumer);
+    const greatestDivisor = gcd(resDenom, resNumer);
+    return [resNumer/greatestDivisor, resDenom/greatestDivisor];
+};
 
 function add_fractions() {
-    var v1,v2,v3,v4 = get_inputs()
+    var v1 = document.getElementById("input1").value;
+    var v2 = document.getElementById("input2").value;
+    var v3 = document.getElementById("input3").value;
+    var v4 = document.getElementById("input4").value;
     var vl = [[v1, v2], [v3, v4]];
     console.log(vl);
     var result= sumFrac(vl[0], vl[1]);
+    document.getElementById("output1").value=result[0];
+    document.getElementById("output2").value=result[1];
+}
+
+
+
+
+function substract_fractions() {
+    var v1 = document.getElementById("input1").value;
+    var v2 = document.getElementById("input2").value;
+    var v3 = document.getElementById("input3").value;
+    var v4 = document.getElementById("input4").value;
+    var vl = [[v1, v2], [v3, v4]];
+    console.log(vl);
+    var result= substractFrac(vl[0], vl[1]);
     document.getElementById("output1").value=result[0];
     document.getElementById("output2").value=result[1];
 }
