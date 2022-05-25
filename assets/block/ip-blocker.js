f6c2cb6ffa539acac9840e5094f26c40b5fe6e54
ALEIXMINE <87977748+ALEIXMINE@users.noreply.github.com>
2022-05-25 15:01:30 +0200

    master

--- assets/block/ip-blocker.js
+++ assets/block/ip-blocker.js
@@ -1,1 +1,18 @@
-// Pendiente de hacer
+const DATA=[];
+  
+$.get('https://aleixmine.github.io/assets/block/ip-blacklist.txt', function(data) {
+data = data.split('\n')
+for (let d in data) {DATA.push(data[d].replace("\r",""));}
+});
+
+$.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+    data = data.trim().split('\n').reduce(function(obj, pair) {
+      pair = pair.split('=');
+      return obj[pair[0]] = pair[1], obj;
+    }, {});
+    for (let ip of DATA) {
+      if (data["ip"] == ip) {
+        document.write("<h1>IP Blocked</h1>");
+      }
+    }
+});
