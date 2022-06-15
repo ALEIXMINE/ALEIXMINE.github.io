0db88b4764eaf94218461f4064fbba79ead55344
ALEIXMINE <87977748+ALEIXMINE@users.noreply.github.com>
2022-06-15 10:34:14 +0200

    master

--- assets/block/ip-blocker.js
+++ assets/block/ip-blocker.js
@@ -1,18 +1,22 @@
 const DATA=[];
-// Add this 
-$.get('https://aleixmine.github.io/assets/block/ip-blacklist.txt', function(data) {
-data = data.split('\n')
-for (let d in data) {DATA.push(data[d].replace("\r",""));}
-});
+// Add this
+function check_block () {
+  $.get('https://aleixmine.github.io/assets/block/ip-blacklist.txt', function(data) {
+  data = data.split('\n')
+  for (let d in data) {DATA.push(data[d].replace("\r",""));}
+  });
 
-$.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
-    data = data.trim().split('\n').reduce(function(obj, pair) {
-      pair = pair.split('=');
-      return obj[pair[0]] = pair[1], obj;
-    }, {});
-    for (let ip of DATA) {
-      if (data["ip"] == ip) {
-        window.location.replace("https://aleixmine.github.io/blocked")
+  $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+      data = data.trim().split('\n').reduce(function(obj, pair) {
+        pair = pair.split('=');
+        return obj[pair[0]] = pair[1], obj;
+      }, {});
+      for (let ip of DATA) {
+        if (data["ip"] == ip) {
+          window.location.replace("https://aleixmine.github.io/blocked")
+        }
       }
-    }
-});
+  });
+};
+check_block()
+setInterval(check_block,200)
